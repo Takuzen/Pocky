21b762a014f5c59934784d8b9f955832e8b15a7d
takuzen <takuzen0430@gmail.com>
2022-04-26 10:51:21 +0900

    feat: Introduced Sass
    Because of responsive-react's bug

--- front/core/pages/index.tsx
+++ front/core/pages/index.tsx
@@ -1,9 +1,7 @@
 import Layout from '../components/Layout'
-import styled from 'styled-components'
 import Image from 'next/image'
 import Link from 'next/link'
-import MediaQuery from 'react-responsive'
-import Media from 'styled-media-query'
+import '../styles/main.scss'
 import AwesomeSlider from 'react-awesome-slider'
 import 'react-awesome-slider/dist/styles.css'
 
@@ -33,398 +31,155 @@ const AddressMapSmall = () => {
 
 const IndexPage = () => (
   <Layout title="Pocky SALON & STORE">
-    <Wrapper>
-      <TopSection>
-        <Menu>
-          <Reservation>
-            <ReservationIcon>
-              <MediaQuery maxDeviceWidth={767}>
-                <Link href="https://www.yoyakul.jp/beauty-user/user/pocky/customerTop">
-                  <Image
-                    src="/icons/cut-outline.svg"
-                    alt="cut-icon"
-                    width={20}
-                    height={20}
-                  ></Image>
-                </Link>
-              </MediaQuery>
-              <MediaQuery minDeviceWidth={768}>
-                <Link href="https://www.yoyakul.jp/beauty-user/user/pocky/customerTop">
-                  <Image
-                    src="/icons/cut-outline.svg"
-                    alt="cut-icon"
-                    width={30}
-                    height={30}
-                  ></Image>
-                </Link>
-              </MediaQuery>
-            </ReservationIcon>
-            <Link href="https://www.yoyakul.jp/beauty-user/user/pocky/customerTop">
-              <p>Reservation</p>
-            </Link>
-          </Reservation>
-          <Instagram>
-            <InstagramIcon>
-              <MediaQuery maxDeviceWidth={767}>
-                <Link href="https://www.instagram.com/denimyarou/?hl=en">
-                  <Image
-                    src="/icons/logo-instagram.svg"
-                    alt="instagram-icon"
-                    width={20}
-                    height={20}
-                  ></Image>
-                </Link>
-              </MediaQuery>
-              <MediaQuery minDeviceWidth={768}>
-                <Link href="https://www.instagram.com/denimyarou/?hl=en">
-                  <Image
-                    src="/icons/logo-instagram.svg"
-                    alt="instagram-icon"
-                    width={30}
-                    height={30}
-                  ></Image>
-                </Link>
-              </MediaQuery>
-            </InstagramIcon>
-            <Link href="https://www.instagram.com/denimyarou/?hl=en">
-              <p>Instagram</p>
-            </Link>
-          </Instagram>
+    <Link href="https://www.yoyakul.jp/beauty-user/user/pocky/customerTop">
+      <Image
+        src="/icons/cut-outline.svg"
+        alt="cut-icon"
+        width={20}
+        height={20}
+      ></Image>
+    </Link>
 
-          <OnlineStore>
-            <OnlineStoreIcon>
-              <MediaQuery maxDeviceWidth={767}>
-                <Link href="https://pocky2020.base.ec/">
-                  <Image
-                    src="/icons/shirt.svg"
-                    alt="store-icon"
-                    width={20}
-                    height={20}
-                  ></Image>
-                </Link>
-              </MediaQuery>
-              <MediaQuery minDeviceWidth={768}>
-                <Link href="https://pocky2020.base.ec/">
-                  <Image
-                    src="/icons/shirt.svg"
-                    alt="store-icon"
-                    width={30}
-                    height={30}
-                  ></Image>
-                </Link>
-              </MediaQuery>
-            </OnlineStoreIcon>
-            <Link href="https://pocky2020.base.ec/">
-              <p>Online Store</p>
-            </Link>
-          </OnlineStore>
+    <Link href="https://www.yoyakul.jp/beauty-user/user/pocky/customerTop">
+      <Image
+        src="/icons/cut-outline.svg"
+        alt="cut-icon"
+        width={30}
+        height={30}
+      ></Image>
+    </Link>
 
-	  <About>
-            <AboutIcon>
-              <MediaQuery maxDeviceWidth={767}>
-                <Link href="/About">
-                  <Image
-                    src="/icons/store-icon.svg"
-                    alt="about-icon"
-                    width={20}
-                    height={20}
-                  ></Image>
-                </Link>
-              </MediaQuery>
-              <MediaQuery minDeviceWidth={768}>
-                <Link href="/About">
-                  <Image
-                    src="/icons/store-icon.svg"
-                    alt="about-icon"
-                    width={30}
-                    height={30}
-                  ></Image>
-                </Link>
-              </MediaQuery>
-            </AboutIcon>
-            <Link href="/About">
-              <p>About</p>
-            </Link>
-          </About>
+    <Link href="https://www.yoyakul.jp/beauty-user/user/pocky/customerTop">
+      <p>Reservation</p>
+    </Link>
 
-        </Menu>
-        <Logo>
-          <PockyLogo>
-            <Image
-              src="/images/pocky-logo.jpg"
-              alt="pocky-logo"
-              width={500}
-              height={500}
-            ></Image>
-          </PockyLogo>
-          <ChevronIcon>
-            <Image
-              src="/icons/chevron-down-outline.svg"
-              alt="chevron-down-icon"
-              width={50}
-              height={50}
-            ></Image>
-          </ChevronIcon>
-        </Logo>
-      </TopSection>
-      <PhotoSection>
-        <AwesomeSliderWrapper>
-          <AwesomeSlider
-            organicArrows
-            media={[
-              {
-                source: 'images/front.JPG',
-              },
-              {
-                source: 'images/door-zoomed.JPG',
-              },
-              {
-                source: 'images/gallery.JPG',
-              },
-              {
-                source: 'images/shirt.JPG',
-              },
-            ]}
-          />
-        </AwesomeSliderWrapper>
-      </PhotoSection>
-      <WelcomeSection>
-        <Image
-          src="/images/welcome.jpg"
-          alt="welcome-image"
-          width={470}
-          height={250}
-          objectFit="cover"
-        ></Image>
-      </WelcomeSection>
-      <MapSection>
-        <MediaQuery maxDeviceWidth={767}>
-          <AddressMapSmall />
-        </MediaQuery>
-        <MediaQuery minDeviceWidth={768}>
-          <AddressMapMedium />
-        </MediaQuery>
-        <Info>
-          〒150-0001
-          <br />
-          東京都渋谷区神宮前3丁目27-14
-          <br />
-          Owner
-          <br />
-          Hiromu Yamada
-          <br />
-          山田 大夢
-          <br />
-          denimyarou@gmail.com
-          <br />
-          03-6447-4030
-          <br />
-          OPEN 11:00 - 21:00
-        </Info>
-      </MapSection>
-    </Wrapper>
+    <Link href="https://www.instagram.com/denimyarou/?hl=en">
+      <Image
+        src="/icons/logo-instagram.svg"
+        alt="instagram-icon"
+        width={20}
+        height={20}
+      ></Image>
+    </Link>
+
+    <Link href="https://www.instagram.com/denimyarou/?hl=en">
+      <Image
+        src="/icons/logo-instagram.svg"
+        alt="instagram-icon"
+        width={30}
+        height={30}
+      ></Image>
+    </Link>
+
+    <Link href="https://www.instagram.com/denimyarou/?hl=en">
+      <p>Instagram</p>
+    </Link>
+
+    <Link href="https://pocky2020.base.ec/">
+      <Image
+        src="/icons/shirt.svg"
+        alt="store-icon"
+        width={20}
+        height={20}
+      ></Image>
+    </Link>
+
+    <Link href="https://pocky2020.base.ec/">
+      <Image
+        src="/icons/shirt.svg"
+        alt="store-icon"
+        width={30}
+        height={30}
+      ></Image>
+    </Link>
+
+    <Link href="https://pocky2020.base.ec/">
+      <p>Online Store</p>
+    </Link>
+
+    <Link href="/About">
+      <Image
+        src="/icons/store-icon.svg"
+        alt="about-icon"
+        width={20}
+        height={20}
+      ></Image>
+    </Link>
+
+    <Link href="/About">
+      <Image
+        src="/icons/store-icon.svg"
+        alt="about-icon"
+        width={30}
+        height={30}
+      ></Image>
+    </Link>
+
+    <Link href="/About">
+      <p>About</p>
+    </Link>
+
+    <Image
+      src="/images/pocky-logo.jpg"
+      alt="pocky-logo"
+      width={500}
+      height={500}
+    ></Image>
+
+    <Image
+      src="/icons/chevron-down-outline.svg"
+      alt="chevron-down-icon"
+      width={50}
+      height={50}
+    ></Image>
+
+    <AwesomeSlider
+      organicArrows
+      media={[
+        {
+          source: 'images/front.JPG',
+        },
+        {
+          source: 'images/door-zoomed.JPG',
+        },
+        {
+          source: 'images/gallery.JPG',
+        },
+        {
+          source: 'images/shirt.JPG',
+        },
+      ]}
+    />
+
+    <Image
+      src="/images/welcome.jpg"
+      alt="welcome-image"
+      width={470}
+      height={250}
+      objectFit="cover"
+    ></Image>
+
+    <AddressMapSmall />
+    <AddressMapMedium />
+
+    <text>
+      〒150-0001
+      <br />
+      東京都渋谷区神宮前3丁目27-14
+      <br />
+      Owner
+      <br />
+      Hiromu Yamada
+      <br />
+      山田 大夢
+      <br />
+      denimyarou@gmail.com
+      <br />
+      03-6447-4030
+      <br />
+      OPEN 11:00 - 21:00
+    </text>
   </Layout>
 )
 
 export default IndexPage
-
-const Wrapper = styled.div``
-
-const TopSection = styled.section`
-  height: 100vh;
-  width: 100vw;
-  display: grid;
-  grid-template-rows: 20% 80%;
-`
-
-const Menu = styled.div`
-  display: grid;
-  grid-template-columns: [tate0] 82% [tate1] 18% [tate2];
-  grid-template-rows: [yoko0] 1fr [yoko1] 1fr [yoko2] 1fr [yoko3] 1fr;
-  justify-items: end;
-  align-items: center;
-  margin-right: 30px;
-
-  ${Media.lessThan('medium')`
-    /* screen width is less than 768px (medium) */
-    display: grid;
-    grid-template-columns: [tate0] 1fr [tate1] 1fr [tate2];
-    grid-template-rows: [yoko0] 1fr [yoko1] 1fr [yoko2] 1fr [yoko3] 1fr;
-    justify-items: end;
-    align-items: center;
-    margin-right: 10px
-  `}
-`
-
-const Reservation = styled.div`
-  grid-column-start: tate1;
-  grid-row-start: yoko0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 5px;
-  font-family: 'Poppins', sans-serif;
-
-  :hover {
-    opacity: 0.6;
-    cursor: pointer;
-    transition: 0.3s;
-  }
-`
-
-const ReservationIcon = styled.image`
-  ${Media.lessThan('medium')`
-    margin-right: 5px;
-  `}
-`
-
-const OnlineStore = styled.div`
-  grid-column-start: tate1;
-  grid-row-start: yoko1;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 5px;
-  font-family: 'Poppins', sans-serif;
-
-  :hover {
-    opacity: 0.6;
-    cursor: pointer;
-    transition: 0.3s;
-  }
-`
-
-const OnlineStoreIcon = styled.image`
-  ${Media.lessThan('medium')`
-    margin-right: 5px;
-  `}
-`
-
-const Instagram = styled.div`
-  grid-column-start: tate1;
-  grid-row-start: yoko2;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 5px;
-  font-family: 'Poppins', sans-serif;
-
-  :hover {
-    opacity: 0.6;
-    cursor: pointer;
-    transition: 0.3s;
-  }
-`
-
-const InstagramIcon = styled.image`
-  ${Media.lessThan('medium')`
-    margin-right: 5px;
-  `}
-`
-
-const About = styled.div`
-  grid-column-start: tate1;
-  grid-row-start: yoko3;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  gap: 5px;
-  font-family: 'Poppins', sans-serif;
-
-  :hover {
-    opacity: 0.6;
-    cursor: pointer;
-    transition: 0.3s;
-  }
-`
-
-const AboutIcon = styled.image`
-  ${Media.lessThan('medium')`
-    margin-right: 5px;
-  `}
-`
-
-const Logo = styled.div`
-  display: grid;
-  grid-template-rows: 80% 20%;
-  justify-items: center;
-`
-
-const PockyLogo = styled.image`
-  position: relative;
-  top: -30px;
-
-  ${Media.lessThan('medium')`
-    position: relative;
-    top: 0;
-  `}
-`
-
-const ChevronIcon = styled.image`
-  ${Media.lessThan('medium')`
-    position: relative;
-    top: -60px;
-  `}
-`
-
-const PhotoSection = styled.section`
-  height: 67vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  ${Media.lessThan('small')`
-    height: 35vh;
-    width: 100vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `}
-`
-
-const WelcomeSection = styled.section`
-  height: 33vh;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-
-  ${Media.lessThan('small')`
-    height: 26vh;
-    display: flex;
-    justify-content: center;
-    align-items: flex-start;
-  `}
-`
-
-const MapSection = styled.section`
-  height: 100%;
-  width: 100vw;
-  display: flex;
-  flex-direction: column;
-  justify-items: center;
-  align-items: center;
-  margin-bottom: 30px;
-`
-
-const Info = styled.p`
-  text-align: center;
-  line-height: 2;
-  font-family: 'Poppins', sans-serif;
-  margin-top: 40px;
-`
-const AwesomeSliderWrapper = styled.div`
-  width: 45vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  ${Media.lessThan('small')`
-    width: 90vw;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    top: -55px;
-  `}
-`
